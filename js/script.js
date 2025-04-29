b1acfdee71580b512c052b2c0cb4299691442b50
reham <reham344@gmail.com>
2025-04-30 00:53:09 +0300

    edit

--- js/script.js
+++ js/script.js
@@ -22,6 +22,25 @@ $(".inputs").keyup(function () {
       
     };
 
+
+    //////////////////////////////// add to cart counter  /////////////////////////////////////////
+
+$('.minus').click(function () {
+  var $input = $(this).parent().find('input');
+  var count = parseInt($input.val()) - 1;
+  count = count < 1 ? 1 : count;
+  $input.val(count);
+  $input.change();
+  return false;
+});
+$('.plus').click(function () {
+  var $input = $(this).parent().find('input');
+  $input.val(parseInt($input.val()) + 1);
+  $input.change();
+  return false;
+});
+
+
    ///////// ** upload images ** /////////
 
 
